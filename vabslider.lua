83e14c64540cebfc37b2c5bc9f9c9f0228925b53
Sam Lee <slee@jwplayer.com>
2018-06-20 17:24:41 -0400

    asdf

---
+++ vabslider.lua
@@ -0,0 +1,17 @@
+local mouseX = 0;
+local mouseY = 0;
+
+local function onTick(event)
+	local x;
+	local y;
+	
+	x, y = mp.get_mouse_pos();
+	if x ~= mouseX or y ~= mouseY then
+		print(x, y);
+		mouseX, mouseY = x, y;
+		mp.command('vf-command xcrop x ' .. mouseX .. '"');
+	end
+end
+
+mp.register_event('tick', onTick);
+mp.set_property('lavfi-complex', '[vid1][vid2]scale2ref[main][ref]; [main]crop@xcrop=w=200:x=0:y=0:keep_aspect=1[cropped]; [ref][cropped]overlay[vo]');
