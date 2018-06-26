e3a87033b6aac604d3ed1fa0d16bdc66d4bc3247
Sam Lee <slee@jwplayer.com>
2018-06-26 14:15:38 -0400

    asdf

--- run.bash
+++ run.bash
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-~/.local/bin/mpv different_720.mp4 --video-unscaled --script vabslider.lua '--vf=@crop:lavfi="movie=720.mp4[larger]; [larger][vid1]scale2ref[scaled][main]; [main]crop=out_w=400:x=0:y=0:keep_aspect=1[cropped]; [scaled][cropped]overlay[vo]"'
+#~/.local/bin/mpv different_720.mp4 --video-unscaled --script vabslider.lua '--vf=@crop:lavfi="movie=720.mp4[larger]; [larger][vid1]scale2ref[scaled][main]; [main]crop=out_w=400:x=0:y=0:keep_aspect=1[cropped]; [scaled][cropped]overlay[vo]"'
+
+
+if (( $# < 2 ))
+then
+	echo "Usage: $0 vid1 vid2"
+	exit 1
+fi
+
+#~/.local/bin/mpv "$1" --external-file "$2" --video-unscaled --script vabslider.lua
+~/.local/bin/mpv "$1" --external-file "$2" --no-osc --script vabslider.lua
--- vabslider.lua
+++ vabslider.lua
@@ -1,20 +1,50 @@
-local mouseX = 0;
-local mouseY = 0;
-
-local function onTick(event)
-	local x;
-	local y;
-	
-	x, y = mp.get_mouse_pos();
-	if x ~= mouseX or y ~= mouseY then
-		print(x, y);
-		mouseX, mouseY = x, y;
-		-- mp.command('vf-command xcrop x ' .. mouseX .. '"');
-		-- mp.commandv('vf-command', 'crop', 'w', '' .. mouseX .. '');
-		-- mp.commandv('vf-command', 'crop', 'out_w', '100');
-		mp.command('vf-command crop out_w 100');
+local _M = {
+	vid1=nil,
+	vid2=nil,
+	larger=nil,
+	smaller=nil,
+	lavfi=nil,
+};
+local function resetSlider()
+	local x, y = mp.get_mouse_pos();
+	local osd_width,_ = mp.get_osd_size();
+	local osd_left_margin,_ = mp.get_osd_margins();
+	local scaled_width = osd_width - osd_left_margin;
+	local video_width = _M.smaller['demux-w'];
+	local scaled_x = x - osd_left_margin;
+	local video_x = video_width * scaled_x / scaled_width;
+	local crop_width;
+	if video_x <= 0 then
+		crop_width = 5;
+	elseif video_x >= video_width then
+		crop_width = video_width;
+	else
+		crop_width = video_x;
 	end
+	print(string.format('mouse=(%s,%s) osd-size=%s osd-margins=%s video_x=%s crop=%s', x, y, osd_width, osd_left_margin, video_x, crop_width));
+	mp.set_property('lavfi-complex', string.format(_M.lavfi, crop_width));
+		
 end
 
-mp.register_event('tick', onTick);
+
+local function main(event)
+	local vid1 = mp.get_property_native('track-list/0');
+	local vid2 = mp.get_property_native('track-list/1');
+	_M.vid1 = vid1;
+	_M.vid2 = vid2;
+	if vid1['demux-w'] * vid1['demux-h'] < vid2['demux-w'] * vid2['demux-h'] then
+		_M.smaller = vid1;
+		_M.larger = vid2;
+		_M.lavfi = '[vid2][vid1]scale2ref[second][first]; [first]crop=w=%s:x=0:y=0[cropped]; [second][cropped]overlay[vo]';
+	else
+		_M.smaller = vid2;
+		_M.larger = vid1;
+		_M.lavfi = '[vid1][vid2]scale2ref[second][first]; [first]crop=w=%s:x=0:y=0[cropped]; [second][cropped]overlay[vo]';
+	end
+	mp.set_property('lavfi-complex', string.format(_M.lavfi, _M.smaller['demux-w'] / 2));
+end
+	
+mp.register_event('file-loaded', main);
+mp.add_key_binding('z', 'resetSlider', resetSlider);
+-- mp.register_event('mbtn_left', onClick);
 -- mp.set_property('lavfi-complex', '[vid1][vid2]scale2ref[main][ref]; [main]crop@xcrop=w=200:x=0:y=0:keep_aspect=1[cropped]; [ref][cropped]overlay[vo]');
